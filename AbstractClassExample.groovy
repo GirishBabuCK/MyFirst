555e593fa1a642f5d299823a0e9a9c182f472a31
GirishBabuCK <girishkrish-rx@LAPTOP-9H70R0JC.localdomain>
2019-03-04 16:55:45 +0530

    practicegit2: commiting after add 2 files

---
+++ AbstractClassExample.groovy
@@ -0,0 +1,32 @@
+class AbstractClassExample { 
+   static void main(String[] args) { 
+      StudentAb st = new StudentAb(); 
+      st.StudentID = 1;
+		
+      st.Marks1 = 10; 
+      st.name="Joe"; 
+		
+      println(st.name); 
+      println(st.DisplayMarks()); 
+   } 
+} 
+
+abstract class PersonAb { 
+   public String name; 
+   public PersonAb() { } 
+   abstract void DisplayMarks();
+}
+ 
+class StudentAb extends PersonAb { 
+   int StudentID 
+   int Marks1; 
+	
+   public StudentAb() { 
+      super(); 
+   } 
+	
+   void DisplayMarks() { 
+      println(Marks1); 
+   }  
+} 
+
